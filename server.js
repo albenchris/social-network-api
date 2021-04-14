084c413d5dcc4162620866ba79e2e2868aa19ca6
albenchris <albenchris00@gmail.com>
2021-04-14 17:26:11 -0500

    initial server setup

---
+++ server.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const chalk = require('chalk');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use(require('./routes'));
+
+
+
+app.listen(PORT, () => console.log(`Now connected to ${chalk.cyanBright(`localhost:${PORT}`)}`));
