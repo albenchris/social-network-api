ed13d3331d2644b8dcfa35522e940d7046ba72f1
albenchris <albenchris00@gmail.com>
2021-04-15 09:31:33 -0500

    initial setup of Thought model with Reaction subdocument schema

---
+++ models/Thought.js
@@ -0,0 +1,59 @@
+const { Schema, model, Types } = require('mongoose');
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            // get: (value) => formatDate(value)
+        },
+        username: {
+            type: String,
+            required: true
+        }
+    }
+)
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            // minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            // get: (value) => formatDate(value)
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        // reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+// virtuals go here
+// create virtual for reactionCount
+
+const Thought = model('Thought', ThoughtSchema);
+
+module.exports = Thought;
